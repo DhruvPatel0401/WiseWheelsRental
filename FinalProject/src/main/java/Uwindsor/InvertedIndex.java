3bddb3309daf3ed01740a46349bda05735e52e1f
Karan Tushar <tusharkaran99@gmail.com>
2024-04-03 18:17:40 -0400

    new changes for regex in inverted index

--- FinalProject/src/main/java/Uwindsor/InvertedIndex.java
+++ FinalProject/src/main/java/Uwindsor/InvertedIndex.java
@@ -2,6 +2,8 @@ package Uwindsor;
 
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class InvertedIndex {
     // a list of terms that won't be indexed
@@ -49,6 +51,7 @@ public class InvertedIndex {
             while ((line = reader.readLine()) != null) {
                 String[] words = line.split("\\s+");
                 for (String word : words) {
+                    word = removeSpecialCharacters(word);
                     if (!EXCLUDED_WORDS.contains(word)) {
                         invertedIndex.computeIfAbsent(word, k -> new HashSet<>()).add(file.getName());
                     }
@@ -57,6 +60,14 @@ public class InvertedIndex {
         } catch (IOException e) {
             System.out.println("Error occurred while indexing file: " + e.getMessage());
         }
+    }
+
+    private String removeSpecialCharacters(String word) {
+        // Define the pattern for identifying special characters
+        Pattern pattern = Pattern.compile("[^a-zA-Z0-9]");
+        Matcher matcher = pattern.matcher(word);
+        // Replace all special characters with an empty string
+        return matcher.replaceAll("");
     }
 
     // Technique for doing a keyword-based information search
@@ -128,8 +139,11 @@ public class InvertedIndex {
             for (Map.Entry<String, Set<String>> entry : invertedData.entrySet()) {
                 if (entry.getKey().equalsIgnoreCase(lowercaseKeyword)) { // Comparison without regard to case
                     Set<String> filesData = entry.getValue();
+                    int count = 0;
                     for (String fileName : filesData) {
-                        dataShowByPrinting(outputFilePathData + "/" + fileName); // Before printing, add the file path.
+                        dataShowByPrinting(outputFilePathData + "/" + fileName, count); // Before printing, add the file
+                                                                                        // path.
+                        count = count + 1;
                     }
                     break;
                 }
@@ -138,13 +152,14 @@ public class InvertedIndex {
     }
 
     // read data from a file and print it
-    public static void dataShowByPrinting(String filePath) {
+    public static void dataShowByPrinting(String filePath, int count) {
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
-            System.out.println("Content of file: " + filePath);
+            System.out.println("Car Data : " + count);
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
+            count += 1;
             System.out.println("----------------------------------");
         } catch (IOException ey) {
             System.out.println("Error occurred while reading file: " + ey.getMessage());
